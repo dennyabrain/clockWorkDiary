a34a2fa675062355cbec3bc2f9b3d259599bba29
dennyabrain <denny.george90@gmail.com>
2016-01-15 13:50:50 -0500

    included utils.py and the function

--- clock.py
+++ clock.py
@@ -1,6 +1,7 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from rq import Queue
 from worker import conn
+from utils import printSomething
 
 q = Queue(connection=conn)
 sched = BlockingScheduler()
@@ -8,6 +9,6 @@ sched = BlockingScheduler()
 @sched.scheduled_job('interval', seconds=5)
 def pollTurk():
 	print('this job runs every 5 seconds')
-	result = q.enqueue(count_words_at_url, 'http://heroku.com')
+	result = q.enqueue(printSomething)
 
 sched.start()
