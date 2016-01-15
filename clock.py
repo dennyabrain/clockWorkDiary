2f18a24a2a00775a989f952202b9b804e98db87d
dennyabrain <denny.george90@gmail.com>
2016-01-15 13:48:37 -0500

    added Worker process

--- clock.py
+++ clock.py
@@ -1,9 +1,13 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
+from rq import Queue
+from worker import conn
 
+q = Queue(connection=conn)
 sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', seconds=5)
 def pollTurk():
 	print('this job runs every 5 seconds')
+	result = q.enqueue(count_words_at_url, 'http://heroku.com')
 
 sched.start()
